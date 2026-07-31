9ac9f45d5bf5890bb3aa32c1c9860244aadd9c3b
JorisJurkenas <joris.jurkenas@stud.vilniustech.lt>
2026-07-31 13:44:47 +0300

    Ar patinka dabar? Pakeiciau visuose failuose rankiniu budu nors jauciu galima paprasciau

--- registracija/registracija.js
+++ registracija/registracija.js
@@ -1,5 +1,16 @@
 const registrationForm = document.querySelector("#registration-form");
 
+const menuButton = document.querySelector(".menu-button");
+const navLinks = document.querySelector(".nav-links");
+
+menuButton.addEventListener("click", () => {
+  navLinks.classList.toggle("open");
+
+  const menuIsOpen = navLinks.classList.contains("open");
+  menuButton.setAttribute("aria-expanded", menuIsOpen);
+  menuButton.textContent = menuIsOpen ? "✕" : "☰";
+});
+
 const ageStatusInputs = document.querySelectorAll(
   'input[name="studentAgeStatus"]'
 );
